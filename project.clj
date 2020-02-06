0112e949b72c1d35344609fefb602583d1f5da70
Brandon Olivier <brandon@brandonolivier.com>
2020-02-06 08:00:17 -0600

    Turn off security features
    They don't seem to be working.  Idk why.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject bolivier-web/lein-template "0.1.0"
+(defproject bolivier-web/lein-template "0.1.1"
   :description "FIXME: write description"
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :sign-releases false}]]
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
