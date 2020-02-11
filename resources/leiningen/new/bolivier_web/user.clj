3f4113617e544e1280b1ed5fb89cca03e9299e44
Brandon Olivier <brandon@brandonolivier.com>
2020-02-10 22:20:35 -0600

    Add clojurescript  repl functions to user ns

--- resources/leiningen/new/bolivier_web/user.clj
+++ resources/leiningen/new/bolivier_web/user.clj
@@ -29,3 +29,21 @@
 
 (defn create-migration [name]
   (migratus/create config name))
+
+;; _____ _       _                              _       _
+;;/  __ | |     (_)                            (_)     | |
+;;| /  \| | ___  _ _   _ _ __ ___ ___  ___ _ __ _ _ __ | |_
+;;| |   | |/ _ \| | | | | '__/ _ / __|/ __| '__| | '_ \| __|
+;;| \__/| | (_) | | |_| | | |  __\__ | (__| |  | | |_) | |_
+;; \____|_|\___/| |\__,_|_|  \___|___/\___|_|  |_| .__/ \__|
+;;             _/ |                              | |
+;;            |__/                               |_|
+
+(defn start-cljs []
+  (server/start!))
+
+(defn watch-cljs []
+  (shadow/watch :frontend))
+
+(defmacro switch-to-cljs-repl []
+  (shadow/repl :frontend))
