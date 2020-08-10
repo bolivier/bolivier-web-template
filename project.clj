c05e0d8bab9f381a2cfd6f8ba8a5d5ca6022f895
Brandon Olivier <brandon@brandonolivier.com>
2020-08-10 15:58:23 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bolivier-web/lein-template "0.1.6"
+(defproject bolivier-web/lein-template "0.1.8"
   :description "My own configuration for web projects."
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :creds :gpg}]]
